fb40b5643d3c3d2d3ac0a18f205bb1f2584595c7
Mihnea Rezmerita <mihnea.superbaiat@gmail.com>
2021-04-17 16:53:29 +0300

    fix for square

--- be/src/main/java/css/be/service/operationImpl/SquareRootCalculatorService.java
+++ be/src/main/java/css/be/service/operationImpl/SquareRootCalculatorService.java
@@ -1,21 +1,12 @@
 package css.be.service.operationImpl;
 
-import css.be.controllers.model.OperandsBody;
-import css.be.exceptions.SquareRootFormatException;
-import css.be.service.OperationCalculatorService;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 @Component
-public class SquareRootCalculatorService implements OperationCalculatorService {
+public class SquareRootCalculatorService{
 
-    @Override
-    public String operate(OperandsBody numbers) {
-        if(!StringUtils.isEmpty(numbers.getSecondNumber()) || numbers.getFirstNumber() == null){
-            throw new SquareRootFormatException("Square root format not respected! First number should not be null! There should be no second number!");
-        }
-
-        return String.format("Executing square on %s", numbers.getFirstNumber());
+    public String operate(String number) {
+        return String.format("Executing square on %s", number);
     }
 
 }
